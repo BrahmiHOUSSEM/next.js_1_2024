bbcd7661817b1f01abb9348704f55b6d5fe69266
BrahmiHOUSSEM <brhhsm55@gmail.com>
2024-04-26 13:17:16 +0100

    image

--- src/app/components/menu/MenuItem.tsx
+++ src/app/components/menu/MenuItem.tsx
@@ -4,7 +4,12 @@ const MenuItem = () => {
   return (
     <div className="bg-gray-300 rounded-lg p-4 text-center hover:bg-gray-100 hover:shadow-md hover:shadow-black/50 duration-300 ease-in-out">
       <div className="flex justify-center">
-        <Image src="/tacos.png" width={300} height={300} alt="TACOS IMAGE" />
+        <Image
+          src="/public/tacos.png"
+          width={300}
+          height={300}
+          alt="TACOS IMAGE"
+        />
       </div>
       <h3 className="uppercase font-bold text-xl py-2">tacos</h3>
       <p className="text-gray-700 text-sm my-2">
